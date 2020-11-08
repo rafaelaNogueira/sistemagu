cb5a3532b98bdab9482b9cc35716258ffd162813
rafaelaNogueira <rafah_nogueira@hotmail.com>
2020-11-08 10:35:54 -0300

    Adicionando backend

---
+++ backend/src/main/java/br/com/zetta/sistemagu/SistemaguApplication.java
@@ -0,0 +1,13 @@
+package br.com.zetta.sistemagu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaguApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/zetta/sistemagu/SistemaguApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zetta.sistemagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
